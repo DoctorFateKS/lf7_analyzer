a1f466d3df3c05a9a5876cb210884867098505b8
Kevin Saison <kevin.saison91@gmail.com>
2025-08-22 14:42:50 +0200

    Rubocop style correction

--- lib/lf7_analyzer.rb
+++ lib/lf7_analyzer.rb
@@ -5,7 +5,7 @@ module LF7Analyzer
   # Application is the main entry point for running analysis tasks.
   class Application
     def open_csv(path)
-      CSV.open(path, "r")
+      CSV.open(path, 'r')
     end
   end
 end
--- spec/lf7_analyzer_spec.rb
+++ spec/lf7_analyzer_spec.rb
@@ -4,12 +4,10 @@ require 'lf7_analyzer'
 require 'csv'
 
 RSpec.describe LF7Analyzer::Application do
-
-  describe "#open_csv" do
-    it "open the csv file without error" do
+  describe '#open_csv' do
+    it 'open the csv file without error' do
       app = LF7Analyzer::Application.new
-      expect { app.open_csv("data/historical_data.csv") }.not_to raise_error
+      expect { app.open_csv('data/historical_data.csv') }.not_to raise_error
     end
   end
-
 end
