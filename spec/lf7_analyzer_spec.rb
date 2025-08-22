884d89c3a3bd205995d6d819a5a236c022534283
Kevin Saison <kevin.saison91@gmail.com>
2025-08-22 14:30:48 +0200

    Implementation of Rspec Test to open_csv method

--- spec/lf7_analyzer_spec.rb
+++ spec/lf7_analyzer_spec.rb
@@ -1,9 +1,15 @@
 # frozen_string_literal: true
 
 require 'lf7_analyzer'
+require 'csv'
 
 RSpec.describe LF7Analyzer::Application do
-  it 'opens the csv file whitout error' do
-    expect
+
+  describe "#open_csv" do
+    it "open the csv file without error" do
+      app = LF7Analyzer::Application.new
+      expect { app.open_csv("data/historical_data.csv") }.not_to raise_error
+    end
   end
+
 end
