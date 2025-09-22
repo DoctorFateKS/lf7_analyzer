d1867b90d34c85bcb0fb73b690e3584f11db8b11
Kevin Saison <kevin.saison91@gmail.com>
2025-09-22 13:20:23 +0200

    Rubocop corrections

--- spec/lf7_analyzer_spec.rb
+++ spec/lf7_analyzer_spec.rb
@@ -3,9 +3,9 @@
 require 'lf7_analyzer'
 
 RSpec.describe LF7 do
-  it "returns the correct score for a random grid (array)" do
+  it 'returns the correct score for a random grid (array)' do
     lf7 = LF7.new
-    score = lf7.compute_score(["1","1","N","1","2","1","1"])
-    expect(score).to eq(26102)
+    score = lf7.compute_score(%w[1 1 N 1 2 1 1])
+    expect(score).to eq(26_102)
   end
 end
