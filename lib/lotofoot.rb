b14fc6b906295b95520eb659e29d80378d9fce8a
KevinS <kevin.saison91@gmail.com>
2025-11-07 20:35:40 +0100

    Calculation of a grid statistics

--- lib/lotofoot.rb
+++ lib/lotofoot.rb
@@ -24,6 +24,91 @@ def sauvegarder_historique(historique)
 end
 
 # Programme principal
+def normaliser_grille(chaine)
+  chaine.tr("-", " ").split.map(&:upcase)
+end
+
+def compter_consecutifs(grille, signe)
+  max = 0
+  actuel = 0
+
+  grille.each do |s|
+    if s == signe
+      actuel += 1
+      max = [max, actuel].max
+    else
+      actuel = 0
+    end
+  end
+
+  max
+end
+
+def compter_diagonales(grille)
+  motifs = [
+    ["1", "N", "2"],
+    ["2", "N", "1"]
+  ]
+  count = 0
+
+  (0..grille.size - 3).each do |i|
+    triplet = grille[i, 3]
+    count += 1 if motifs.include?(triplet)
+  end
+
+  count
+end
+
+def compter_symetries(grille)
+  count = 0
+  left = 0
+  right = grille.size - 1
+
+  while left < right
+    count += 1 if grille[left] == grille[right]
+    left += 1
+    right -= 1
+  end
+
+  count
+end
+
+def compter_alternances(grille)
+  count = 0
+  (0..grille.size - 2).each do |i|
+    count += 1 if grille[i] != grille[i + 1]
+  end
+  count
+end
+
+def compter_series(grille, longueur)
+  series = []
+  (0..grille.size - longueur).each do |i|
+    series << grille[i, longueur].join
+  end
+  series.uniq.size
+end
+
+def calculer_stats(grille)
+  {
+    "nombre_1" => grille.count("1"),
+    "nombre_N" => grille.count("N"),
+    "nombre_2" => grille.count("2"),
+
+    "cons_1" => compter_consecutifs(grille, "1"),
+    "cons_N" => compter_consecutifs(grille, "N"),
+    "cons_2" => compter_consecutifs(grille, "2"),
+
+    "diagonales" => compter_diagonales(grille),
+    "symetries" => compter_symetries(grille),
+    "alternances" => compter_alternances(grille),
+
+    "paires"  => compter_series(grille, 2),
+    "tierces" => compter_series(grille, 3),
+    "quarts"  => compter_series(grille, 4)
+  }
+end
+
 def afficher_menu
   puts "\n===== LOTOFOOT 7 ====="
   puts '1. Afficher la grille du jour'
@@ -40,8 +125,13 @@ def boucle_principale(_historique)
     case choix
     when '1'
       puts "\n[INFO] Fonction pas encore implémentée. La grille du jour sera affichée ici."
-    when '2'
-      puts "\n[INFO] Fonction pas encore implémentée. L'ajout de grille sera ici."
+    when "2"
+      print "\nEntrez la grille (ex: 1-2-1-N-1-2-2): "
+      saisie = gets.chomp
+      g = normaliser_grille(saisie)
+      stats = calculer_stats(g)
+      puts "\nStatistiques calculées:"
+      puts stats
     when '3'
       puts "\nAu revoir."
       break
